7bb135f10ed46f92691b1e829b08e1c9dd2ca2b1
khurram khan <153985864+khurramkhan0786@users.noreply.github.com>
2025-02-20 15:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	let result = '';
+   for(let char of s){
+	   if(char === ' '){
+		  break; 
+	   }
+	   else{
+		   result = result+char
+	   }
+   }
+	console.log(result);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
